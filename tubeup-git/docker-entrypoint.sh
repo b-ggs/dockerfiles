689b27c0d8ce1a6c2ec8c236ccee517ef1f39286
boggs <hello@boggs.xyz>
2021-02-11 22:30:51 +0800

    fix entrypoint

--- tubeup-git/docker-entrypoint.sh
+++ tubeup-git/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -n $S3_ACCESS ] && [ -n $S3_SECRET ]; then
+if [ -n "$S3_ACCESS" ] && [ -n "$S3_SECRET" ]; then
   sed -i "s/s3_access/$S3_ACCESS/g" /root/.ia
   sed -i "s/s3_secret/$S3_SECRET/g" /root/.ia
 
