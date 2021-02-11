424446540b16308c7f366881b63a4b6e8ef47699
boggs <hello@boggs.xyz>
2021-02-11 16:58:27 +0800

    update tubeup docker-entrypoint

--- tubeup-git/docker-entrypoint.sh
+++ tubeup-git/docker-entrypoint.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-sed -i "s/s3_access/$S3ACCESS/g" /root/.ia
-sed -i "s/s3_secret/$S3SECRET/g" /root/.ia
+if [ -n $S3_ACCESS ] && [ -n $S3_SECRET ]; then
+  sed -i "s/s3_access/$S3_ACCESS/g" /root/.ia
+  sed -i "s/s3_secret/$S3_SECRET/g" /root/.ia
 
-tubeup $1
+  exec tubeup "$@"
+else
+  echo "Please supply S3_ACCESS and S3_SECRET via environment variables" 1>&2;
+fi
