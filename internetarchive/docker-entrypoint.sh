ca145e33f11226a7c843887695f013f27395dd42
boggs <hello@boggs.xyz>
2021-11-02 06:25:18 +0800

    Add internetarchive

---
+++ internetarchive/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+if [ -n "$S3_ACCESS" ] && [ -n "$S3_SECRET" ]; then
+  sed -i "s/s3_access/$S3_ACCESS/g" /root/.ia
+  sed -i "s/s3_secret/$S3_SECRET/g" /root/.ia
+
+  exec ia "$@"
+else
+  echo "Please supply S3_ACCESS and S3_SECRET via environment variables" 1>&2;
+fi
